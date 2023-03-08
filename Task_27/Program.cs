877666ba9a32cecd55b7486fbbb875ac7a6838f6
Yasha <yashasidin333@yandex.ru>
2023-03-08 15:20:18 +0300

    Add task_27

--- Task_27/Program.cs
+++ Task_27/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetSumDigits(int number)
+{
+    string allDigits = number.ToString();
+    int sum = 0;
+    for (int i = 0; i < allDigits.Length; i++)
+    {
+        int toSum = number % 10;
+        sum = sum + toSum;
+    }
+
+    return sum;
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Сумма чисел равна: {GetSumDigits(number)}");
